9a01d119e4cce6e026131124963a2f881f2d8d92
Matt <algoworldnft@gmail.com>
2023-05-13 21:49:20 +0200

    fix: link fixed to processed_notes.json

--- src/components/Headers/NavBar.tsx
+++ src/components/Headers/NavBar.tsx
@@ -223,7 +223,7 @@ const NavBar = () => {
 
   /*  */
   const lastblockUrl = React.useMemo(() => {
-    return `https://raw.githubusercontent.com/AlgoWorldNFT/algoworld-workers/testnet/data/${selectedChain.toLowerCase()}/aw_build/metadata.json`;
+    return `https://raw.githubusercontent.com/AlgoWorldNFT/algoworld-workers/${selectedChain.toLowerCase()}/data/${selectedChain.toLowerCase()}/aw_build/metadata.json`;
   }, [selectedChain]);
 
   const lastblockResponse = useSWR(lastblockUrl, (url: string) => {
